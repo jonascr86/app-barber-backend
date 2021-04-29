f10f69e8e88e1d37f5bc6d4dd55671a3257c7a78
jonascr86 <jonascr86@gmail.com>
2021-04-28 22:09:50 -0300

    corrigindo erro de casting

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -34,7 +34,7 @@ class AppointmentsRepository implements IAppoitmentsRepository{
       where:{
         provider_id,
         date: Raw(dateFieldName =>
-          `to_char(${dateFieldName}, 'MM-YYYY') = ${parsedMonth}-${year}`),
+          `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`),
       }
     })
     return appointments;
@@ -48,7 +48,7 @@ class AppointmentsRepository implements IAppoitmentsRepository{
       where:{
         provider_id,
         date: Raw(dateFieldName =>
-          `to_char(${dateFieldName}, 'DD-MM-YYYY') = ${parsedDay}-${parsedMonth}-${year}`),
+          `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`),
       }
     })
     return appointments;
